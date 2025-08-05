2ce4917c7d6bc8d809b130c24ebbe2f1ab75cca0
Mariana <marianaongay.01@gmail.com>
2025-08-05 12:23:55 -0600

    Mostrar propiedades POO

--- classes/ActiveRecord.php
+++ classes/ActiveRecord.php
@@ -46,6 +46,14 @@ class ActiveRecord {
 
         return $resultado;
     }
+    //obtiene determinado numero de registros
+    public static function get($cantidad) {
+        $query = "SELECT * FROM " . static::$tabla . " LIMIT " . $cantidad;
+
+        $resultado = self::consultarSQL($query);
+
+        return $resultado;
+    }
 
     // Busca un registro por su id
     public static function find($id) {
